3c9faaf2d76590d5106b65eea668be1059ddc84f
digvijaykhatri <digvijaykhatri@hotmail.com>
2025-03-29 17:14:56 +0530

    added a program to convert temprature from c to f

--- celciustoferehnheit.java
+++ celciustoferehnheit.java
@@ -5,12 +5,11 @@ public class celciustoferehnheit {
     public static void main(String[] args) {
         
         System.out.println("Enter temprature in celcius: ");
-        Scanner obj = new Scanner(System.in);
-        int x = obj.nextInt();
+        try (Scanner obj = new Scanner(System.in)) {
+            int x = obj.nextInt();
 
-        int y = (x*9/5)+32;
-        System.out.println("Temprature in ferehnheit is: "+y);
-
-        
-        }
+            int y = (x*9/5)+32;
+            System.out.println("Temprature in ferehnheit is: "+y);
+        }       
     }
+}
