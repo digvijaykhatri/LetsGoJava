865ebf74c11c463d9db6a54c2722085b835f1a62
digvijaykhatri <digvijaykhatri@hotmail.com>
2025-03-29 17:18:57 +0530

    added program to check validity of a triangle

--- checktrianglevalidity.java
+++ checktrianglevalidity.java
@@ -3,25 +3,32 @@ import java.util.Scanner;
 public class checktrianglevalidity {
 
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
+        try(Scanner obj = new Scanner(System.in);){
+
         System.out.println("enter the first side of the triangle: ");
         int a = obj.nextInt();
+
         System.out.println("enter the second side of the triangle: ");
         int b = obj.nextInt();
+
         System.out.println("enter the third side of the triangle: ");
         int c = obj.nextInt();
-        if (a + b > c && b + c > a && c + a > b) {
+
+            if (a + b > c && b + c > a && c + a > b) {
             System.out.println("the triangle is valid.");
             if (a == b && b == c) {
                 System.out.println("the triangle is equilateral.");
+
             } else if (a == b || b == c || c == a) {
                 System.out.println("the triangle is isosceles.");
+
             } else {
                 System.out.println("the triangle is scalene.");
             }
-        } else {
+            
+            } else {
             System.out.println("the triangle is invalid.");
+            }
         }
-        obj.close();
     }
 }
